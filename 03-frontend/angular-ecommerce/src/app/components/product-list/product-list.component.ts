bd9f45c088134e542f29e586bb5758c9af24ee0e
Ian An <qan001@uchicago.edu>
2022-01-01 16:07:25 -0600

    Dynamically display products based on the categories

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -23,13 +23,19 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+
+    // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
     }
     else {
+      // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
+
+    // now get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
