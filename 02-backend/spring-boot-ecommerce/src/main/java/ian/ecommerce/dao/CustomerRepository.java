d314effe84cb8e6088853c08bcd8267b3fda8e98
Ian An <qan001@uchicago.edu>
2022-01-02 20:31:13 -0600

    Save order details to database: create jpa repository for REST API

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/ian/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package ian.ecommerce.dao;
+
+import ian.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
