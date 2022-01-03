42b4d760f4e929c7b9d59f2ba59869ae379e3cab
Ian An <qan001@uchicago.edu>
2022-01-02 20:57:43 -0600

    Save order details to database: create controller

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/ian/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package ian.ecommerce.controller;
+
+import ian.ecommerce.dto.Purchase;
+import ian.ecommerce.dto.PurchaseResponse;
+import ian.ecommerce.service.CheckoutService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin("http://localhost:4200")
+@Controller
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+}
