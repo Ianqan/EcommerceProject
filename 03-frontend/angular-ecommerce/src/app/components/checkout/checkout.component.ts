786c49c57ce1d60ef4981d3780b614d37254e0e6
Ian An <qan001@uchicago.edu>
2022-01-02 19:50:53 -0600

    review section on the checkout page, apply behavior subject (instead of replay subject) to get the previously published information

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
@@ -25,9 +26,11 @@ export class CheckoutComponent implements OnInit {
   billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
-              private luv2ShopFormService: Luv2ShopFormService) { }
+              private luv2ShopFormService: Luv2ShopFormService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
@@ -79,6 +82,10 @@ export class CheckoutComponent implements OnInit {
         this.countries = data;
       }
     )
+
+    // subscribe total price and quantity to the behavior subject in the cart service
+    // get values for previously published messages
+    this.reviewCartDetails();
   }
 
   onSubmit() {
@@ -158,6 +165,20 @@ export class CheckoutComponent implements OnInit {
           this.billingAddressStates = data;
         }
         formGroup?.get('state')?.setValue(data[0]);
+      }
+    );
+  }
+
+  reviewCartDetails() {
+    // subscription
+    this.cartService.totalPrice.subscribe(
+      data => {
+        this.totalPrice = data;
+      }
+    );
+    this.cartService.totalQuantity.subscribe(
+      data => {
+        this.totalQuantity = data;
       }
     );
   }
